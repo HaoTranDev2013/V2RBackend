74d26c4d342efb7228774cd4bf910c9d11d85e08
HaoTranDev2013 <trananhhao225@gmail.com>
2025-11-21 12:13:18 +0700

    fix v1

--- src/main/java/com/v2r/v2rbackend/config/CorsConfig.java
+++ src/main/java/com/v2r/v2rbackend/config/CorsConfig.java
@@ -20,7 +20,8 @@ public class CorsConfig {
                 "http://localhost:3000",
                 "http://localhost:3001",
                 "http://localhost:4200",
-                "http://127.0.0.1:3000"
+                "http://127.0.0.1:3000",
+                "https://vision2reality.vercel.app"
         ));
         
         // Allow all HTTP methods
